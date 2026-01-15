ba9f7d0310d0b93f8a1b3ab265566a7b828261c7
xweinp <xweinp@gmail.com>
2026-01-15 14:45:46 +0100

    replace sorting with medain of medians

--- leader/src/main/java/com/graph/dist/leader/Shard.java
+++ leader/src/main/java/com/graph/dist/leader/Shard.java
@@ -92,28 +92,27 @@ public class Shard {
      * Uses median to split nodes as evenly as possible.
      */
     private Shard[] splitVertically(Map<Integer, Point> coords) {
-        // Sort nodes by x coordinate
-        List<Integer> sortedNodes = new ArrayList<>(nodeIds);
-        sortedNodes.sort((a, b) -> Integer.compare(coords.get(a).x, coords.get(b).x));
-
-        // Split at median index (middle of sorted list)
-        int medianIndex = sortedNodes.size() / 2;
+        List<Integer> nodes = new ArrayList<>(nodeIds);
+        int medianIndex = nodes.size() / 2;
         
         // Ensure we don't create empty shards
-        assert medianIndex > 0 && medianIndex < sortedNodes.size();
+        assert medianIndex > 0 && medianIndex < nodes.size();
         
-        List<Integer> leftNodes = new ArrayList<>(sortedNodes.subList(0, medianIndex));
-        List<Integer> rightNodes = new ArrayList<>(sortedNodes.subList(medianIndex, sortedNodes.size()));
+        // Partition nodes around the median index using Median of Medians
+        select(nodes, 0, nodes.size() - 1, medianIndex, (a, b) -> Integer.compare(coords.get(a).x, coords.get(b).x));
+        
+        List<Integer> leftNodes = new ArrayList<>(nodes.subList(0, medianIndex));
+        List<Integer> rightNodes = new ArrayList<>(nodes.subList(medianIndex, nodes.size()));
 
         // Find the actual coordinate bounds for each new shard
         int leftXMax = xMin;
         int rightXMin = xMax;
         
         if (!leftNodes.isEmpty()) {
-            leftXMax = coords.get(leftNodes.get(leftNodes.size() - 1)).x;
+            leftXMax = getMaxCoordinate(leftNodes, coords, true);
         }
         if (!rightNodes.isEmpty()) {
-            rightXMin = coords.get(rightNodes.get(0)).x;
+            rightXMin = getMinCoordinate(rightNodes, coords, true);
         }
 
         // Use midpoint between the last left node and first right node as boundary
@@ -130,28 +129,27 @@ public class Shard {
      * Uses median to split nodes as evenly as possible.
      */
     private Shard[] splitHorizontally(Map<Integer, Point> coords) {
-        // Sort nodes by y coordinate
-        List<Integer> sortedNodes = new ArrayList<>(nodeIds);
-        sortedNodes.sort((a, b) -> Integer.compare(coords.get(a).y, coords.get(b).y));
-
-        // Split at median index (middle of sorted list)
-        int medianIndex = sortedNodes.size() / 2;
+        List<Integer> nodes = new ArrayList<>(nodeIds);
+        int medianIndex = nodes.size() / 2;
         
         // Ensure we don't create empty shards
-        assert medianIndex > 0 && medianIndex < sortedNodes.size();
+        assert medianIndex > 0 && medianIndex < nodes.size();
         
-        List<Integer> bottomNodes = new ArrayList<>(sortedNodes.subList(0, medianIndex));
-        List<Integer> topNodes = new ArrayList<>(sortedNodes.subList(medianIndex, sortedNodes.size()));
+        // Partition nodes around the median index using Median of Medians
+        select(nodes, 0, nodes.size() - 1, medianIndex, (a, b) -> Integer.compare(coords.get(a).y, coords.get(b).y));
+        
+        List<Integer> bottomNodes = new ArrayList<>(nodes.subList(0, medianIndex));
+        List<Integer> topNodes = new ArrayList<>(nodes.subList(medianIndex, nodes.size()));
 
         // Find the actual coordinate bounds for each new shard
         int bottomYMax = yMin;
         int topYMin = yMax;
         
         if (!bottomNodes.isEmpty()) {
-            bottomYMax = coords.get(bottomNodes.get(bottomNodes.size() - 1)).y;
+            bottomYMax = getMaxCoordinate(bottomNodes, coords, false);
         }
         if (!topNodes.isEmpty()) {
-            topYMin = coords.get(topNodes.get(0)).y;
+            topYMin = getMinCoordinate(topNodes, coords, false);
         }
 
         // Use midpoint between the last bottom node and first top node as boundary
@@ -161,6 +159,86 @@ public class Shard {
         Shard topShard = new Shard(xMin, xMax, splitY + 1, yMax, topNodes);
 
         return new Shard[] { bottomShard, topShard };
+    }
+
+    // Median of Medians Selection Algorithm
+    private int select(List<Integer> list, int left, int right, int k, java.util.Comparator<Integer> comp) {
+        while (left <= right) {
+            int pivotIndex = pivot(list, left, right, comp);
+            int partitionIndex = partition(list, left, right, pivotIndex, comp);
+            if (k == partitionIndex) {
+                return k;
+            } else if (k < partitionIndex) {
+                right = partitionIndex - 1;
+            } else {
+                left = partitionIndex + 1;
+            }
+        }
+        return k;
+    }
+
+    private int pivot(List<Integer> list, int left, int right, java.util.Comparator<Integer> comp) {
+        if (right - left < 5) {
+            return medianOfFive(list, left, right, comp);
+        }
+        
+        int storeIndex = left;
+        for (int i = left; i <= right; i += 5) {
+            int subRight = Math.min(i + 4, right);
+            int median5 = medianOfFive(list, i, subRight, comp);
+            swap(list, median5, storeIndex);
+            storeIndex++;
+        }
+        
+        return select(list, left, storeIndex - 1, left + (storeIndex - 1 - left) / 2, comp);
+    }
+
+    private int medianOfFive(List<Integer> list, int left, int right, java.util.Comparator<Integer> comp) {
+        // Insertion sort for small range
+        for (int i = left + 1; i <= right; i++) {
+            for (int j = i; j > left && comp.compare(list.get(j), list.get(j - 1)) < 0; j--) {
+                swap(list, j, j - 1);
+            }
+        }
+        return (left + right) / 2;
+    }
+
+    private int partition(List<Integer> list, int left, int right, int pivotIndex, java.util.Comparator<Integer> comp) {
+        Integer pivotValue = list.get(pivotIndex);
+        swap(list, pivotIndex, right);
+        int storeIndex = left;
+        for (int i = left; i < right; i++) {
+            if (comp.compare(list.get(i), pivotValue) <= 0) {
+                swap(list, i, storeIndex);
+                storeIndex++;
+            }
+        }
+        swap(list, storeIndex, right);
+        return storeIndex;
+    }
+    
+    private void swap(List<Integer> list, int i, int j) {
+        Integer temp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, temp);
+    }
+
+    private int getMaxCoordinate(List<Integer> nodes, Map<Integer, Point> coords, boolean isX) {
+        int max = Integer.MIN_VALUE;
+        for (Integer id : nodes) {
+            int val = isX ? coords.get(id).x : coords.get(id).y;
+            if (val > max) max = val;
+        }
+        return max;
+    }
+
+    private int getMinCoordinate(List<Integer> nodes, Map<Integer, Point> coords, boolean isX) {
+        int min = Integer.MAX_VALUE;
+        for (Integer id : nodes) {
+            int val = isX ? coords.get(id).x : coords.get(id).y;
+            if (val < min) min = val;
+        }
+        return min;
     }
 
     @Override
